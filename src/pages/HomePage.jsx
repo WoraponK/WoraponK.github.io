e340b7bf492e4662ca6a104fc5217473f417a849
WoraponK <worapon.klabsri@gmail.com>
2023-12-29 00:29:11 +0700

    add: technology icons

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -16,7 +16,25 @@ const HomePage = () => {
             <p className='indent-6 max-sm:indent-0'>I'm an undergraduate student majoring in Software Engineering at University of Phayao.</p>
             <p className='indent-6 max-sm:indent-0'>Ready to learn new things about software development, and excited to join your team.</p>
           </div>
-          <img src="images/woraponk-nobg.png" alt="woraponk-profile" className='w-[480px] grayscale max-lg:w-[360px]' />
+          <img src="images/woraponk-nobg.png" alt="woraponk-profile" className='w-[480px] max-lg:w-[360px]' />
+        </div>
+      </section>
+      <section className='bg-base-content text-base-100 py-8 grid place-items-center space-y-6'>
+        <h2 className='text-2xl font-semibold'>Technologies I use & love</h2>
+        <div className='flex items-center space-x-5 max-sm:flex-col max-sm:space-x-0 max-sm:space-y-4'>
+          <div className='flex items-center space-x-5'>
+            <img className="h-[40px]" src="icons/html.png"/>
+            <img className="h-[40px]" src="icons/css.png"/>
+            <img className="h-[40px]" src="icons/javascript.png"/>
+            <img className="h-[40px]" src="icons/typescript.png"/>
+            <img className="h-[40px]" src="icons/angular.png"/>
+          </div>
+          <div className='flex items-center space-x-5'>
+            <img className="h-[40px]" src="icons/vue.png"/>
+            <img className="h-[40px]" src="icons/react.png"/>
+            <img className="h-[30px]" src="icons/tailwindcss.png"/>
+            <img className="h-[30px] invert" src="icons/daisyui.png"/>
+          </div>
         </div>
       </section>
       <ProjectComponent />
