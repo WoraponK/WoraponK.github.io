789e3136df793c5f78779ef0cb53d00efc1ea730
WoraponK <worapon.klabsri@gmail.com>
2023-12-29 01:20:58 +0700

    fix: title

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -12,7 +12,7 @@ const HomePage = () => {
       <section className='grid place-items-center max-sm:mt-8'>
         <div className='flex justify-center items-center w-3/4 max-xl:w-full max-xl:px-8 max-lg:px-4 max-sm:flex-col max-sm:space-y-4'>
           <div className='text-3xl font-semibold max-lg:text-2xl max-sm:text-center max-sm:text-xl'>
-            <h2 className='text-4xl font-bold text-primary mb-2 max-lg:text-3xl max-sm:text-2xl'>Hi! I'm Worapon Klabsri</h2>
+            <h2 className='text-4xl font-bold text-primary mb-2 max-lg:text-3xl max-sm:text-2xl'>Hi! My name is Worapon Klabsri</h2>
             <p className='indent-6 max-sm:indent-0'>I'm an undergraduate student majoring in Software Engineering at University of Phayao.</p>
             <p className='indent-6 max-sm:indent-0'>Ready to learn new things about frontend development, and excited to join your team.</p>
           </div>
